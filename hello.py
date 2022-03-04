53e04322a5bc811d8c30f69ae58654c1723f84bf
Katherine Kampf <acrokat@github.com>
2022-03-03 18:38:42 -0800

    fix tests

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def hello_world():
-    return("Hello world")
+    print("Hello world")
     
 def cats_or_dogs():
-    return("cats AND dogs")
+    print("cats AND dogs")
 
 if __name__ == '__main__':
     hello_world()
